9804ca97c5d51bb35b91f67ba55cb314b43d7abe
Unknown <dereksnakespam@gmail.com>
2020-02-28 23:11:38 -0500

    adding basic text dialog class (functional), making git ignore compiled

--- tyr.cpp
+++ tyr.cpp
@@ -38,6 +38,9 @@ class Editor : protected Window{
         Editor(int h, int w, int y0, int x0){
             Window::create_windows(h, w, y0, x0);
         }
+        WINDOW* getWindow(){
+            return Window::win;
+        }
         Cursor cursor;
         void resize(int, int);
 
@@ -62,7 +65,28 @@ class Dialog : protected Window{
     protected:
     public:
         Dialog(std::string str){
-            
+            int len = str.size();
+            // get the size of stdscr (fills terminal, so basically get terminal size)
+            int screen_rows, screen_cols;
+            getmaxyx(stdscr, screen_rows, screen_cols);
+            //screen_rows = LINES;
+            //screen_cols = COLS;
+            // assuming that we want the max width to be 1/3 of the screen (idk i just picked this #)
+            int width = std::min(len, (screen_cols + 2)/3); // this looks jank but stack overflow says this will round the division up
+            // determine the number of rows
+            int height;
+            if (len < width){
+                height = 1;
+            } else{
+                height = len/width + (int) (len % width != 0);
+            }
+            // center dialog box
+            int start_x = (screen_cols + 1)/2 - (width + 1)/2; // again, rounding up the division
+            int start_y = (screen_rows + 1)/2 - (height + 1)/2;
+            // offsets to account for the border
+            Window::create_windows(height + 2, width + 2, start_y - 1, start_x - 1);
+            waddstr(Window::win, str.data());
+            wrefresh(Window::win);
         }
 };
 
@@ -72,7 +96,6 @@ void focusOnFileViewer(FileViewer fs){
     curs_set(0);
     // refresh the file viewer window
     wrefresh(fs.getWindow());
-
 }
 
 int main() {
@@ -87,6 +110,7 @@ int main() {
     // creates the editor screen
     Editor ed (LINES-1, COLS-20, 0, 20);
     FileViewer fs (LINES-1, 21, 0, 0);
+    Dialog dia ("how's this???? is this enough lines to trigger wrap yet????");
     //update the panel stacking
     update_panels();
     doupdate();
