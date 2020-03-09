518c74ac4c9db7910940a2a72d6b3c781fe46095
derekthesnake <evanguenterberg@gmail.com>
2020-03-09 10:49:21 -0400

    more work on adding string buffers
    also made ed and fs globals

--- tyr.cpp
+++ tyr.cpp
@@ -17,6 +17,8 @@ struct Cursor {
     int line_num, line_position;
 };
 
+int screen_rows, screen_cols;
+
 class Window {
     protected:
         int width, height;
@@ -45,6 +47,12 @@ class Window {
 class Editor : protected Window{
     protected:
     public:
+        // number of elements in strs
+        // if this is 0, strs is empty
+        // this means that strs[strs_size] is out of bounds
+        // strs[strs_size - 1] is the last element
+        int strs_size;
+
         Editor(int h, int w, int y0, int x0){
             Window::create_windows(h, w, y0, x0);
             cursor = Cursor();
@@ -52,6 +60,7 @@ class Editor : protected Window{
             cursor.screen_y = 0;
             mvwaddstr(Window::border_win, cursor.screen_y+1, 1, std::to_string(1).c_str());
             *strs = new std::string*[1];
+            strs_size = 1;
             *strs[0] = new std::string();
         }
 
@@ -65,29 +74,60 @@ class Editor : protected Window{
         void onFocus();
 
         void handleInput(int c){
+            return;
             switch (c){
                 case KEY_RIGHT:
-                    if((&strs[cursor.line_num]).size() > cursor.screen_x){ // check if its valid to move over a character
+                    if((*(*strs)[cursor.line_num]).size() > cursor.screen_x){ // check if its valid to move over a character
                         cursor.screen_x += 1;
                         cursor.line_position += 1;
                         wmove(Window::win, cursor.screen_y, cursor.screen_x);
+                    } else {
+                        // TODO proper restriction here: case of being at the bottom of the screen with more below, offscreen
+                        cursor.screen_x = 0;
+                        cursor.screen_y += 1;
+                        cursor.line_position = 0;
+                        cursor.line_num += 1;
                     };
                         // if not, do nothing
                         // TODO: try to shift the line over to continue the view.
                     break;
                 case KEY_LEFT:
-                    cursor.screen_x -= 1;
-                    cursor.line_position -= 1;
+                    if(cursor.line_position == 0){
+                        if(cursor.line_num != 0){
+                            cursor.line_position = (*(*strs)[cursor.line_num]).size() - 1;
+                            cursor.line_num -= 1;
+                            cursor.screen_x = std::min(screen_cols, cursor.line_position);
+                        }
+                    } else {
+                        cursor.line_position -= 1;
+                        cursor.screen_x -= 1;
+                    }
                     wmove(Window::win, cursor.screen_y, cursor.screen_x);
                     break;
                 case KEY_UP:
-                    cursor.screen_y -= 1;
-                    cursor.line_num -= 1;
+                    if(cursor.line_num != 0){
+                        cursor.line_num -= 1;
+                        cursor.line_position = std::min((int)(*(*strs)[cursor.line_num]).size() - 1, cursor.line_position);
+                        cursor.screen_y -= 1;
+                        cursor.screen_x = std::min(screen_cols, cursor.line_position);
+                    } else if (cursor.line_position != 0){
+                        cursor.line_position = 0;
+                        cursor.screen_x = 0;
+                    }
                     wmove(Window::win, cursor.screen_y, cursor.screen_x);
                     break;
                 case KEY_DOWN:
-                    cursor.screen_y += 1;
-                    cursor.line_num += 1;
+                    if(cursor.line_num == strs_size - 1){ // on last line
+                        if(cursor.line_position != (*(*strs)[cursor.line_num]).size() - 1){
+                            cursor.line_position = (*(*strs)[cursor.line_num]).size() - 1;
+                            cursor.screen_x = std::min((int) (*(*strs)[cursor.line_num]).size() - 1, screen_cols);
+                        }
+                    } else {
+                        cursor.line_num += 1;
+                        cursor.screen_y = std::min(cursor.line_num, screen_rows);
+                        cursor.line_position = std::min(cursor.line_position, (int) (*(*strs)[cursor.line_num]).size() - 1);
+                        cursor.screen_x = std::min(cursor.line_position, screen_cols);
+                    }
                     wmove(Window::win, cursor.screen_y, cursor.screen_x);
                     break;
                 case 10: // ENTER KEY
@@ -119,6 +159,7 @@ class Editor : protected Window{
         // i'm using a pointer to an array because it allows for dynamic length of the array
         // i'm using an array of pointers because copying pointers from one array to another should be 100x
         // more efficient than copying full strings
+        // to access an element of this shit, use (*(*strs)[n]). im sorry.
         // i can resize strings nicely:
         // see http://www.cplusplus.com/reference/string/string/insert/
         // also, keep this as the last declaration in the class. yes.
@@ -145,7 +186,6 @@ class Dialog : protected Window{
         Dialog(std::string str){
             int len = str.size();
             // get the size of stdscr (fills terminal, so basically get terminal size)
-            int screen_rows, screen_cols;
             getmaxyx(stdscr, screen_rows, screen_cols);
             //screen_rows = LINES;
             //screen_cols = COLS;
@@ -170,6 +210,7 @@ class Dialog : protected Window{
 
 Editor *ed;
 FileViewer *fs;
+
 
 void focusOnFileViewer(FileViewer fs){
     waddstr(fs.getWindow(),"Hey whats poppin gamers");
@@ -197,6 +238,7 @@ int main() {
     cbreak();
     noecho();
     keypad(stdscr, true);
+    getmaxyx(stdscr, screen_rows, screen_cols);
     // creates the editor screen
     ed = new Editor(LINES-1, COLS-20, 0, 20);
     fs = new FileViewer(LINES-1, 21, 0, 0);
